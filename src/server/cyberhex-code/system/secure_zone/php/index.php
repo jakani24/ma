e6380b9369f2c37a1e12e58aad1beb8bf4cc04e9
jakani24 <janis.st44@gmail.com>
2024-01-14 14:49:58 +0100

    Update index.php

--- src/server/cyberhex-code/system/secure_zone/php/index.php
+++ src/server/cyberhex-code/system/secure_zone/php/index.php
@@ -22,7 +22,7 @@ $username="test";
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 	<style>
-		@media (max-width: 500px) {
+		@media (max-width: 50000px) {
 		  #smallScreenWarning {
 			display: block !important;
 		  }
