596c782243b62147ed9d06a9181a828af282f86f
jakani24 <janis.st44@gmail.com>
2024-01-14 13:01:31 +0100

    Update install.php

--- src/server/cyberhex-code/install.php
+++ src/server/cyberhex-code/install.php
@@ -31,15 +31,84 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Create sample tables
+// Create user table
 $sql = "CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL
+    email VARCHAR(255) NOT NULL,
+	perms VARCHAR(255),
+	2fa VARCHAR(255)
 )";
 
 if ($conn->query($sql) === TRUE) {
     echo "Table 'users' created successfully\n";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+// Create log table
+$sql = "CREATE TABLE IF NOT EXISTS log (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    logtext VARCHAR(255) NOT NULL,
+    loglevel VARCHAR(255) NOT NULL,
+	machine_id VARCHAR(255),
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table 'log' created successfully\n";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+// Create settings table
+$sql = "CREATE TABLE IF NOT EXISTS settings (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    value VARCHAR(255) NOT NULL,
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table 'settings' created successfully\n";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+// Create api table
+$sql = "CREATE TABLE IF NOT EXISTS api (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    apikey VARCHAR(255) NOT NULL,
+    machine_id VARCHAR(255) NOT NULL,
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table 'api' created successfully\n";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+// Create secrets table
+$sql = "CREATE TABLE IF NOT EXISTS secrets (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    cert VARCHAR(255) NOT NULL,
+    machine_id VARCHAR(255) NOT NULL,
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table 'secrets' created successfully\n";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+// Create machine table
+$sql = "CREATE TABLE IF NOT EXISTS machines (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    machine_name VARCHAR(255) NOT NULL,
+    machine_location VARCHAR(255) NOT NULL,
+	machine_ip VARCHAR(255) NOT NULL,
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table 'machines' created successfully\n";
 } else {
     echo "Error creating table: " . $conn->error;
 }
