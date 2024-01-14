f001ead1c241b43da2a5374eb7190b24f551a8f3
jakani24 <janis.st44@gmail.com>
2024-01-14 14:37:15 +0100

    Update index.php

--- src/server/cyberhex-code/system/secure_zone/php/index.php
+++ src/server/cyberhex-code/system/secure_zone/php/index.php
@@ -28,10 +28,26 @@ $username="test";
 <body>
 	<!-- Header -->
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-	  <center><h2>Cyberhex <?php echo($page); ?></h2></center>
+	  <a class="navbar-brand" href="#">Navbar w/ text</a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	  </button>
+	  <div class="collapse navbar-collapse" id="navbarText">
+		<ul class="navbar-nav mr-auto">
+		  <li class="nav-item active">
+			<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+		  </li>
+		  <li class="nav-item">
+			<a class="nav-link" href="#">Features</a>
+		  </li>
+		  <li class="nav-item">
+			<a class="nav-link" href="#">Pricing</a>
+		  </li>
+		</ul>
 		<span class="navbar-text">
-		  <a href="/logout.php">Logout</a>
+		  Navbar text with an inline element
 		</span>
+	  </div>
 	</nav>
 	<div class="container mt-5">
 		<!-- Main content -->
