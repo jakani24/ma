1c84b074e46a894edd0c38de031ddf01faff518a
jakani24 <janis.st44@gmail.com>
2024-01-14 16:04:05 +0100

    Create welcome.php

---
+++ src/server/cyberhex-code/system/secure_zone/php/welcome.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+
+// Check if the user is logged in
+if (!isset($_SESSION['username']) or !isset($_SESSION["login"])) {
+    // Redirect to the login page or handle unauthorized access
+    header("Location: /login.php");
+    exit();
+}
+
+$username = $_SESSION['username'];
+$perms = $_SESSION["perms"];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+	 <title>Cyberhex</title>
+</head>
+<body>
+	<h4>Welcome to the Cyberhex dashboard</h4>
+	<p>On your left are all available actions / settings of Cyberhex</p>
+	<p>If you need support, please contact us! <a href="mailto:info.jaakch@gmail.com">info.jakach@gmail.com</a></p>
+</body>
+</html>
