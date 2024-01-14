cc9be049eb90be63bd75197514c36843a88fc591
jakani24 <janis.st44@gmail.com>
2024-01-14 14:50:51 +0100

    Update index.php

--- src/server/cyberhex-code/system/secure_zone/php/index.php
+++ src/server/cyberhex-code/system/secure_zone/php/index.php
@@ -22,12 +22,20 @@ $username="test";
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 	<style>
-		@media (max-width: 50000px) {
+		@media (max-width: 500px) {
 		  #smallScreenWarning {
 			display: block !important;
 		  }
 		}
 	</style>
+	<script>
+	  // Show the warning modal on small screens
+	  $(document).ready(function() {
+		if ($(window).width() <= 500) {
+		  $('#smallScreenWarning').modal('show');
+		}
+	  });
+	</script>
 </head>
 <body>
 	<!-- navbar -->
