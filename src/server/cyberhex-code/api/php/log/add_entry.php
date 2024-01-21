640965528f446a6b4d2ec5a6d7eda2c1b40df81c
jakani24 <janis.st44@gmail.com>
2024-01-21 16:57:08 +0100

    Update add_entry.php

--- src/server/cyberhex-code/api/php/log/add_entry.php
+++ src/server/cyberhex-code/api/php/log/add_entry.php
@@ -6,22 +6,23 @@
 //this page has no gui, it may return ok or error
 if(!isset($_GET["loglevel"]) or !isset($_GET["logtext"]) or !isset($_GET["machine_id"]))
 	echo("syn_err");
-$loglevel=htmlspecialchars($_GET["loglevel"]);
-$logtext=htmlspecialchars($_GET["logtext"]);
-$machine_id=htmlspecialchars($_GET["machine_id"]);
-//include db pw
-include "../../../config.php";
-$conn = new mysqli($DB_SERVERNAME, $DB_USERNAME, $DB_PASSWORD, $DB_DATABASE);
-if ($conn->connect_error) {
-	die("conn_err");
+else{
+	$loglevel=htmlspecialchars($_GET["loglevel"]);
+	$logtext=htmlspecialchars($_GET["logtext"]);
+	$machine_id=htmlspecialchars($_GET["machine_id"]);
+	//include db pw
+	include "../../../config.php";
+	$conn = new mysqli($DB_SERVERNAME, $DB_USERNAME, $DB_PASSWORD, $DB_DATABASE);
+	if ($conn->connect_error) {
+		die("conn_err");
+	}
+	$sql = "INSERT INTO log loglevel,machine_id,logtext VALUES (?,?,?);";
+	$stmt = $conn->prepare($sql);
+	$stmt->bind_param("sss", $$loglevel,$machine_id,$logtext);
+	// Execute the statement
+	if(!$stmt->execute())
+		echo("wrt_err");
+	$stmt->close();
+	$conn->close();
 }
-$sql = "INSERT INTO log loglevel,machine_id,logtext VALUES (?,?,?);";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("sss", $$loglevel,$machine_id,$logtext);
-// Execute the statement
-if(!$stmt->execute())
-	echo("wrt_err");
-$stmt->close();
-$conn->close();
-
 ?>
