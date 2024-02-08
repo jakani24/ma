c440c65073f3814659c804c148bf271e4a7dadb7
jakani24 <janis.st44@gmail.com>
2024-02-08 21:16:10 +0100

    Update index.php

--- src/server/cyberhex-code/system/secure_zone/php/index.php
+++ src/server/cyberhex-code/system/secure_zone/php/index.php
@@ -66,6 +66,7 @@ if(isset($_GET["page"])){
 		  <p>Cyberhex settings</p>
 		  <ul>
 		   <li><a href="index.php?page=server_settings.php">Server Settings</a></li>
+			<li>
 			<p>Client Settings
 			<ul>
 				<li><a href="index.php?page=client_settings.php">General Client Settings</a></li>
@@ -73,6 +74,7 @@ if(isset($_GET["page"])){
 			</ul>
 		   <li><a href="index.php?page=database_settings.php">Database Settings</a></li>
 		  </ul>
+		  </li>
 		  <p>Clients</p>
 		  <ul>
 		   <li><a href="index.php?page=mass_add_clients.php">Mass Add CLients</a></li>
