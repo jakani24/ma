6343af7d971151b6f76582765e130338c90065ad
jakani24 <janis.st44@gmail.com>
2024-02-11 20:32:56 +0100

    Update get_settings.php

--- src/server/cyberhex-code/api/php/settings/get_settings.php
+++ src/server/cyberhex-code/api/php/settings/get_settings.php
@@ -66,6 +66,7 @@ if(isset($_GET["sched"])){
 		$action=$buf[1];
 		$argument=$buf[2];
 		$name=$buf[3];
+		$name=str_replace(" ","_",$name);
 		//e.g.: "43 * * * *" scanfolder "c:\" tsk1
 		echo("\"$time\" $action \"$argument\" $name\n");
 	}
