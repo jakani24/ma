934aec7433fe1ce3d8ef2c0efc445b9a05722c56
jakani24 <janis.st44@gmail.com>
2024-02-11 20:00:45 +0100

    Update client_settings.php

--- src/server/cyberhex-code/system/secure_zone/php/client_settings.php
+++ src/server/cyberhex-code/system/secure_zone/php/client_settings.php
@@ -273,6 +273,20 @@ function load_settings(){
                     <h4>Client settings</h4>
                 </div>
                 <div class="card-body">
+					<ul class="nav nav-tabs">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Active</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+  </li>
+</ul>
 					<h4>General</h4>
 					<!-- Dropdown for virus controll action -->
 					<h7>What should be done, if the scanner finds a virus?</h7>
