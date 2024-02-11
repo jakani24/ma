41af700fd3e1f2d670804bef6cc1dce2baff00e1
jakani24 <janis.st44@gmail.com>
2024-02-11 20:07:20 +0100

    Update client_settings.php

--- src/server/cyberhex-code/system/secure_zone/php/client_settings.php
+++ src/server/cyberhex-code/system/secure_zone/php/client_settings.php
@@ -287,7 +287,7 @@ function load_settings(){
                 <div class="card-body">
 					<ul class="nav nav-tabs">
 					  <li class="nav-item">
-						<a class="nav-link active" href="&show=general" id="general_tab">General Settings</a>
+						<a class="nav-link" href="&show=general" id="general_tab">General Settings</a>
 					  </li>
 					  <li class="nav-item">
 						<a class="nav-link" href="#">Link</a>
