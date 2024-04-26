586ae86027c24f10ebab89acbe8bb70ee79fa6fd
jakani24 <janis.st44@gmail.com>
2024-04-26 14:31:12 +0200

    Update create_acc.php

--- src/server/cyberhex-code/test/create_acc.php
+++ src/server/cyberhex-code/test/create_acc.php
@@ -361,11 +361,14 @@ try {
         $data->userId = $userId;
         $data->userName = $userName;
         $data->userDisplayName = $userDisplayName;
+		$data->email="a@a.com";
+		$data->perms="11111111111111111";
+		$data->password="...";
 
         // Store registration data in the database
-        $stmt = $conn->prepare("INSERT INTO users (username,user_hex_id, credential_id, public_key, counter) VALUES (?,?, ?, ?, ?)");
+        $stmt = $conn->prepare("INSERT INTO users (email,perms,password,username,user_hex_id, credential_id, public_key, counter) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
 		//var_dump($data);
-        $stmt->execute([$data->userName,$userId, $data->credentialId, $data->credentialPublicKey, $data->signatureCounter]);
+        $stmt->execute([$data->email,$data->perms,$data->email, $data->userName, $userId, $data->credentialId, $data->credentialPublicKey, $data->signatureCounter]);
 
         $msg = 'registration success.';
         $return = new stdClass();
