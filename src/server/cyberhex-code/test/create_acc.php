01f2c3488d7c9447dc56eb4058a808d7235fd402
jakani24 <janis.st44@gmail.com>
2024-04-26 14:27:09 +0200

    Update create_acc.php

--- src/server/cyberhex-code/test/create_acc.php
+++ src/server/cyberhex-code/test/create_acc.php
@@ -364,7 +364,7 @@ try {
 
         // Store registration data in the database
         $stmt = $conn->prepare("INSERT INTO users (user_hex_id, credential_id, public_key, counter) VALUES (?, ?, ?, ?)");
-		var_dump($data);
+		//var_dump($data);
         $stmt->execute([$userId, $data->credentialId, $data->credentialPublicKey, $data->signatureCounter]);
 
         $msg = 'registration success.';
