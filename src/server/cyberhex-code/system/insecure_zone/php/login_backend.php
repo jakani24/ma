3e3b9b09111824f429e337e9ede5e49ca4b3b855
jakani24 <janis.st44@gmail.com>
2024-06-06 15:56:01 +0200

    Update login_backend.php

--- src/server/cyberhex-code/system/insecure_zone/php/login_backend.php
+++ src/server/cyberhex-code/system/insecure_zone/php/login_backend.php
@@ -179,7 +179,8 @@ try {
 		$_SESSION["allow_pw_login"]=$row["allow_pw_login"];
 		$_SESSION["send_login_message"]=$row["send_login_message"];
 		if($_SESSION["send_login_message"]=="1"){
-			$ip = $_SERVER['HTTP_CLIENT_IP'];
+			$ip = $_SERVER['REMOTE_ADDR'];
+			$username=$row["username"];
 			send_to_user("[LOGIN WARNING]\nHello $username\nSomebody has logged into Cyberhex with your account.\nIf this was you, you can ignore this message. Else please take steps to secure your account!\nIP: $ip\n",$username);
 		}
 		
